f8ef0a97693c5142e66188c6b811e99cd2bf19b6
Pranit Bauva <pranit.bauva@gmail.com>
2017-09-29 18:27:18 +0530

    exact the start and end point
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,10 +3,44 @@
 using namespace std;
 using namespace cv;
 
+struct bgr_color {
+    int b_low;
+    int b_high;
+    int g_low;
+    int g_high;
+    int r_low;
+    int r_high;
+} green, red;
+
+Mat img, start_img, end_img;
+
+void init_bgrcolor() {
+    green = {0, 10, 200, 255, 0, 10};
+    red = {0, 10, 0, 10, 100, 255};
+}
+
+void extract_color() {
+    inRange(img, Scalar(green.b_low, green.g_low, green.r_low),
+            Scalar(green.b_high, green.g_high, green.r_high), start_img);
+
+    inRange(img, Scalar(red.b_low, red.g_low, red.r_low),
+            Scalar(red.b_high, red.g_high, red.r_high), end_img);
+}
+
+void display_images() {
+    imshow("Area", img);
+    imshow("Start", start_img);
+    imshow("End", end_img);
+}
+
 int main() {
-    Mat img;
     img = imread("a-star-image.jpg", CV_LOAD_IMAGE_COLOR);
-    imshow("Arena", img);
+
+    init_bgrcolor();
+    extract_color();
+
+    display_images();
     waitKey(0);
+
     return 0;
 }
